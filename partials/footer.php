5f9a0c3978da291669b613d3a36efd14581c9084
Matej Budai <matbdi@protonmail.com>
2020-10-02 23:14:42 +0200

    deleted redundant classes from footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,13 +1,13 @@
 <footer>
         <div class="container">
             <ul>
-                <li class="footer_section">
+                <li>
                     <h1>Aboot Us</h1>
                     <span>
                         We are a store that specialises in selling antique items since 1886. Our wide sortiment ranges from baroque table colcks to wooden country chairs. Everything we sell is an authentic period artefact, certified by the International Board for Authenticity of Legacy Items.
                     </span>
                 </li>
-                <li class="footer_section">
+                <li>
                     <h1>Contact Us</h1>
                     <span>
                         Address - 88 Lower Slaughter Saltwater WR9 1HR <br>
@@ -15,9 +15,9 @@
                         Telephone - 024 7312 1547 
                     </span>
                 </li>
-                <li class="footer_section">
+                <li>
                     <h1>Quick Links</h1>
-                    <ul class="footer_links link_wrapper">
+                    <ul class="footer_links">
                         <li><a href="index.php">Home</a></li>
                         <li><a href="cart.php">Shopping Cart</a></li>
                         <li><a href="#">Log In</a></li>
