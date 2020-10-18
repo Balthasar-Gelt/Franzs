c4060664b67a13830681b113578413702fc2d478
Matej Budai <matbdi@protonmail.com>
2020-10-18 12:09:53 +0200

    added cookie disclaimer to header

--- partials/header.php
+++ partials/header.php
@@ -4,7 +4,7 @@ if (session_status() == PHP_SESSION_NONE)
     session_start();
 
 require_once 'nav-menu.php';
-include_once 'variables/baseUrl.php';
+require_once 'variables/baseUrl.php';
 ?>
 
 <html lang="en">
@@ -35,4 +35,5 @@ include_once 'responsive-menu.php';
 include_once 'search.php'; 
 include_once 'log-in-register-forms.php';
 include_once 'messages.php';
+include_once 'cookies-disclaimer.php';
 ?>
