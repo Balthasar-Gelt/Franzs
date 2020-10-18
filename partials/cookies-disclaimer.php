35390597c8e44418ffc264b972bb74cdaf0a9512
Matej Budai <matbdi@protonmail.com>
2020-10-18 12:08:57 +0200

    cookie disclaimer html code

---
+++ partials/cookies-disclaimer.php
@@ -0,0 +1,4 @@
+<div class="disclaimer">
+    <span>This site uses cookies. By using this site you will be providing consent to our use of cookies.</span>
+    <img src="assets/other/remove-24px.svg" alt="delete">
+</div>
