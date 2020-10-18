4c2ca8b166a28565f8533b2cb12026e8b43001a3
Matej Budai <matbdi@protonmail.com>
2020-10-18 13:27:07 +0200

    adde comments annd added item exists check before it is added to cart

--- page-funcs/helper-funcs/cart-action.php
+++ page-funcs/helper-funcs/cart-action.php
@@ -7,23 +7,49 @@ require '../classes/CartManager.php';
 require '../classes/Cart.php';
 require '../classes/DB.php';
 
+// Performs action based action type
+// First checks if item id is INT, and if it exists in db
+// Then checks if selected action is available
+// If both values pass validation then the selected action is performed with sent item id
+
+$db = new DB($dbConfig);
+$cart = new Cart($db);
+$cartManager = new CartManager($cart->getCartItems(), new DB($dbConfig));
+
 $availableActions = ['addToCart', 'deleteFromCart', 'emptyCart'];
-$validations = [];
+$checks = [];
 $response;
 
-$validations['id'] = filter_var($_GET['id'], FILTER_VALIDATE_INT);
-$validations['type'] = in_array($_GET['type'], $availableActions);
+$checks['id'] = checkItemId($_GET['id']);
+$checks['type'] = in_array($_GET['type'], $availableActions);
 
-foreach ($validations as $valid) {
-    
-    if(!$valid)
-        redirect();
-}
-
-$cart = new Cart(new DB($dbConfig));
-$cartManager = new CartManager($cart->getCartItems(), new DB($dbConfig));
+checkArray($checks);
 
 $func = $_GET['type'];
 $response = $cartManager->$func($_GET['id']);
 
 redirect(answer($response));
+
+// FUNCTIONS
+
+function checkItemId($itemId){
+
+    global $db;
+
+    if(filter_var($itemId, FILTER_VALIDATE_INT)){
+
+        if($db->fetchRow('SELECT id FROM PRODUCTS WHERE id =?', [$itemId]) !== false)
+            return true;
+    }
+
+    return false;
+}
+
+function checkArray($array){
+
+    foreach ($array as $check) {
+    
+        if(!$check)
+            redirect();
+    }
+}
