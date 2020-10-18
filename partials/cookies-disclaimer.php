6a2958f1c9676f7074c0a89a89c889d44c3e9991
Matej Budai <matbdi@protonmail.com>
2020-10-18 15:40:30 +0200

    added check for session variable if cookie disclaimer has been closed

--- partials/cookies-disclaimer.php
+++ partials/cookies-disclaimer.php
@@ -1,4 +1,9 @@
-<div class="disclaimer">
-    <span>This site uses cookies. By using this site you will be providing consent to our use of cookies.</span>
-    <img src="assets/other/remove-24px.svg" alt="delete">
-</div>
+<?php
+
+if(!isset($_SESSION['cookies_disclaimer'])){
+
+    echo '<div class="disclaimer">
+            <span>This site uses cookies. By using this site you will be providing consent to our use of cookies.</span>
+            <a href="page-funcs/helper-funcs/cookie-disclaimer-settings.php?disclaimer_disable=true"><img src="assets/other/remove-24px.svg" alt="delete"></a>
+        </div>';
+}
