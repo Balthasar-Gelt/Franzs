d351f4476feea1134876c603d9cb4db157fa5f0e
Matej Budai <matbdi@protonmail.com>
2020-10-18 19:32:39 +0200

    removed img wrapper and changed its position, added overflow scroll to responsive mennu

--- partials/responsive-menu.php
+++ partials/responsive-menu.php
@@ -2,8 +2,6 @@
 
 require_once 'nav-menu.php';
 echo '<div class="menu_overlay">';
-echo '<div class="overlay_img_wrapper">';
 echo '<img src="'.'http://'. $_SERVER['SERVER_NAME'] .':'. $_SERVER['SERVER_PORT'] .'/Dealers/assets/other/clear.svg' .'" alt="Hey, Vsauce, image here">';
-echo '</div>';
 printNavMenu();
 echo '</div>';
